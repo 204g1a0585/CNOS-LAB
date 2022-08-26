7579434047612950023bff11800d65d72aae848e
204g1a0585 <112164162+204g1a0585@users.noreply.github.com>
2022-08-26 03:00:39 -0700

    my 1000 c file is created

--- oslab/sample.c
+++ oslab/sample.c
@@ -2,12 +2,9 @@
 Name of the exprement : Exprement1
 Roll no: 204g1a05855
 Name : harsha
-/*
-body {
-background-color: lightblue;
-}
-
-h1 {
-color: navy;
-margin-left: 20px;
+*/
+#include<stdio.h>
+int main()
+{
+   printf("hello Bro")
 }
